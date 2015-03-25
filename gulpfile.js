0ade91a91fdfc07371c1640c38a9b07b2b211e3e
Ryan <rburgess@evernote.com>
2015-03-25 12:56:29 -0700

    update js and add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp'),
   minifyCSS = require('gulp-minify-css'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  jshint = require('gulp-jshint');
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./'));
+});
 
+gulp.task('jshint', function() {
+  return gulp.src(['js/coderesponsible.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function() {
