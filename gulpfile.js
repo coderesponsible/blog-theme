f418b4d908170d3f70fcfe3bbfd56b311979815d
ryanburgess <ryanb@netflix.com>
2015-08-03 22:52:47 -0700

    start new js using es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var gulp = require('gulp'),
-  minifyCSS = require('gulp-minify-css'),
-  sass = require('gulp-sass'),
-  jshint = require('gulp-jshint');
+var gulp      = require('gulp'),
+  minifyCSS   = require('gulp-minify-css'),
+  sass        = require('gulp-sass'),
+  browserify  = require('gulp-browserify'),
+  uglify      = require('gulp-uglify'),
+  rename      = require('gulp-rename'),
+  replace     = require('gulp-replace'),
+  jshint      = require('gulp-jshint'),
+  path        = require('path');
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
@@ -16,7 +21,23 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('scripts', function() {
+  gulp.src('js/*.js')
+    .pipe(browserify({
+      insertGlobals : true,
+      transform: ['babelify'],
+      debug : !gulp.env.production
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+       extname: '.min.js'
+     }))
+    .pipe(replace('./build/js/*.min.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
 gulp.task('watch', function() {
+  gulp.watch('js/**/*.js', ['jshint', 'scripts']);
   gulp.watch('sass/**/*.scss', ['css']);
 });
 
--- js/coderesponsible.js
+++ js/coderesponsible.js
@@ -1,199 +1,34 @@
-(function($) {
-  'use strict';
+const mobileBreak = '480';
+const inbetween = '600';
+const tabletBreak = '880';
+const searchLink = document.getElementById('search-link');
+const menuLink = document.getElementById('menu-link');
+const mainNav = document.getElementById('main-nav');
+const searchform = document.getElementById('searchform');
 
-  var mobileBreak = '480';
-  var inbetween = '600';
-  var tabletBreak = '880';
-
-  $(window).bind('resize', checkWindowSize);
-	function checkWindowSize(e) {
-		console.log('resize');
-		var windowWidth = $(window).width();
-		if(windowWidth >= inbetween){
-			$('#main-nav').show();
-			$('#searchform').show();
-		}
-		if(windowWidth < inbetween){
-			$('#main-nav').hide();
-			$('#searchform').hide();
-		}
+window.onresize = (event) => {
+	let windowWidth = window.innerWidth;
+	
+	if(windowWidth >= inbetween){
+		mainNav.classList.add('show');
+		mainNav.classList.remove('hide');
+		searchform.classList.add('show');
+		searchform.classList.remove('hide');
+	}else if(windowWidth <= inbetween){
+		mainNav.classList.add('hide');
+		mainNav.classList.remove('show');
+		searchform.classList.add('hide');
+		searchform.classList.remove('show');
 	}
-	checkWindowSize();
-	
-	/* ===================== Form Validation Start ===================== */
-	var RegEx = {
-		name: /^[A-Za-z ,'-]{1,25}$/,
-		email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-		postalcode: /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z][ -.]*[0-9][A-Z][0-9]$/i,
-		message: /./
-	};
-	
-	var errorMsg = {
-		name: 'Name',
-		email: 'Valid Email Required'
-	};
-	
-	//validate email address
-	$(function() {
-		$('.email-submit').on('click', function(e){	
-			var field = $('#email-address').attr('name');
-			if (RegEx[field] !== undefined ) {
-				if(!RegEx[field].test($('#email-address').val()) || $('#email-address').val() === ''){
-					$(this).siblings('span.error-message').html(errorMsg[field]);
-					e.preventDefault();
-				}else{
-					$('#email-address').siblings('span.error-message').fadeOut();
-					$('#email-address').siblings('span.error-message').html('');
-					var emailAddress = 'email=' + $('#email-address').val();
-					//submit email address
-					$.ajax({
-						type: 'POST',
-						url: 'http://coderesponsible.com/dev/wp-content/themes/cr-2012/email-submit.php',
-						data: emailAddress,
-						cache: false,
+};
 
-						success: function(){
-							$('#email-address').val('');
-						}
-					});
-					return false;
-				}
-			}
+let menuOpenCose = () => {
+	console.log('click')
+	if(searchform.offsetParent === null){
+		searchform.classList.add('open');
+	}else{
+		searchform.classList.add('close');
+	}
+}
 
-		});
-	});
-	
-	$('#email-address').on('blur', function(e){	
-		var field = $('#email-address').attr("name");
-		if (RegEx[field] !== undefined ) {
-			if(!RegEx[field].test($('#email-address').val()) || $('#email-address').val() === ''){
-				$(this).siblings('span.error-message').html(errorMsg[field]);
-				e.preventDefault();
-			}else{
-				$('#email-address').siblings('span.error-message').fadeOut();
-				$('#email-address').siblings('span.error-message').html('');
-			}
-		}
-	});
-	
-	// modal windows
-	$('a[name=modal]').on('click' , function(e) {
-		
-		var id = $(this).attr('href');
-    var maskHeight = $(document).height();
-    var maskWidth = $(window).width();
-    $('#mask').css({'width':maskWidth,'height':maskHeight});
-    $('#mask').show();   
-    var winH = $(window).height();
-    var winW = $(window).width();
-		$(id).css('top', ( $(window).height() - $(id).height() ) / 2+$(window).scrollTop() + 'px');
-		$(id).css('left', ( $(window).width() - $(id).width() ) / 2+$(window).scrollLeft() + 'px');
-    $(id).fadeIn();
-		e.preventDefault();
-		return false;
-		
-	});
-	
-	//if close button is clicked
-  $('.window .close').on('click', function (e) {
-    //Cancel the link behavior
-    e.preventDefault();
-    $('#mask, .window').hide();
-  });
-
-	//if mask is clicked
-	$('#mask').on("click", function (e) {
-    //Cancel the link behavior
-    e.preventDefault();
-    $('#mask, .window').hide();
-  });
-
-  // Add class for skip link
-  $('section h2 a:first').addClass('article-title');
-
-	//skip link
-	$('.main-content-link').on('click', function(){
-		$('a.article-title').focus();
-		$('a.article-title').attr('tabindex',0);
-		return false;
-	});
-	
-	// slideout
-	$('a[name=slideout]').on('click', function(e) {
-		var id = $(this).attr('href');
-    $(id).slideDown();
-		
-		e.preventDefault();
-		return false;
-	});
-	
-	//if close button is clicked
-  $('.slideout .close').on('click', function (e) {
-		$(this).parent().slideUp();
-		
-		e.preventDefault();
-		return false;
-  });
-	
-	// Mobile/Tablet navigation menu
-	$('#menu-link').on('click', function(){
-		if($('#main-nav').is(':visible')){
-			$('#main-nav').slideUp();
-		}else{
-			$('#main-nav').slideDown();
-		}
-	});
-
-	// Mobile/Tablet navigation search
-	$('#search-link').on('click', function(){
-		if($('#searchform').is(':visible')){
-			$('#searchform').slideUp();
-		}else{
-			$('#searchform').slideDown();
-		}
-	});
-
-	//Event Tracking
-
-	// Buttons in content
-	$('.content a.button').on('click', function(){
-		var dataAction = $(this).html();
-		var dataLabel = $(this).attr('href');
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-
-	// Social Media
-	//sidebar
-	$('#sidebar .rss').on('click', function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'RSS';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-	$('#sidebar .twitter').on('click', function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'Twitter';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-	$('#sidebar .facebook').on('click', function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'Facebook';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-	//footer
-	$('#footer .rss').on('click', function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'RSS';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-	$('#footer .twitter').on('click', function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'Twitter';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-	$('#footer .facebook').on("click", function(){
-		var dataAction = 'Social Sidebar';
-		var dataLabel = 'Facebook';
-		_gaq.push(['_trackEvent', 'Click Event', dataAction, dataLabel]);
-	});
-}(jQuery));
-
+searchLink.addEventListener('click', menuOpenCose);
